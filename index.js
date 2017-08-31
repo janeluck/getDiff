4c3e82091f6c169aaaf387b8b0d2d46b43986db8
janeluck <janeluck158@gmail.com>
2017-08-31 22:08:22 +0800

    ok

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var Immutable = require('immutable')
-var diff = require('immutablediff')
-var fs = require('fs')
+var Immutable = require('immutable'),
+    diff = require('immutablediff'),
+    fs = require('fs')
 
 // 读取json数据转存为immutable数据结构格式
-var map1 = Immutable.fromJS(JSON.parse(fs.readFileSync('data_old.json')))
-var map2 = Immutable.fromJS(JSON.parse(fs.readFileSync('data_new.json')))
+var map1 = Immutable.fromJS(JSON.parse(fs.readFileSync('data_old.json'))),
+    map2 = Immutable.fromJS(JSON.parse(fs.readFileSync('data_new.json')))
 
 // 输出比较结果
 fs.writeFile('difference.json', JSON.stringify(diff(map1, map2).toJS()))
